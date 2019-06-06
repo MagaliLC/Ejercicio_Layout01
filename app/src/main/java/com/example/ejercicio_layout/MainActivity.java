091e688bd28945c142c39dc9077d32fc1ba7df67
Magalí Lescano Correa <magalilescano@gmail.com>
2019-06-06 13:46:16 +0200

    Añadido el checkbox

--- app/src/main/java/com/example/ejercicio_layout/MainActivity.java
+++ app/src/main/java/com/example/ejercicio_layout/MainActivity.java
@@ -10,27 +10,34 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     EditText etName;
     TextView tv;
+    CheckBox checkSelect;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         etName = findViewById(R.id.name);
         tv = findViewById(R.id.nameOutput);
+        checkSelect = findViewById(R.id.check);
     }
 
-    public void sendMessage (View view){
+    public void sendMessage(View view) {
         String n = etName.getText().toString();
-        tv.setText(getString(R.string.hello)+" " + n + " " +getString(R.string.welcome));
+        if (checkSelect.isChecked()) {
+            tv.setText(getString(R.string.hello) + " " + n + " " + getString(R.string.welcome) + " " + getString(R.string.checked));
+        } else {
+            tv.setText(getString(R.string.hello) + " " + n + " " + getString(R.string.welcome) + " " + getString(R.string.unchecked));
+
+        }
 
     }
 
-    public void clearMessage(View view){
+    public void clearMessage(View view) {
         tv.setText("");
         etName.setText("");
     }
 
-    public void checkedCheckbox(View view){
-        boolean checked = ((CheckBox) view).isChecked();
-
-    }
 }
+
+
+
